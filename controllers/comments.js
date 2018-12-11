45280ea7b724af2f3d69f21d6e605f0ea87163de
Sam Harrison <samuel.harrison@students.makeschool.com>
2018-12-11 00:05:55 -0800

    added authors to post and comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,29 +1,36 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports = app => {
 
     // CREATE Comment
     app.post("/posts/:postId/comments", function(req, res) {
       // INSTANTIATE INSTANCE OF MODEL
-      const comment = new Comment(req.body);
-      console.log(req.params)
+      if (req.user) {
+          const comment = new Comment(req.body);
+          comment.author = req.user._id;
+          console.log(req.params)
 
-      // SAVE INSTANCE OF Comment MODEL TO DB
-      comment
-        .save()
-        .then(comment => {
-          return Post.findById(req.params.postId);
-        })
-        .then(post => {
-          post.comments.unshift(comment);
-          return post.save();
-        })
-        .then(post => {
-          res.redirect(`/posts/${req.params.postId}`);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+          // SAVE INSTANCE OF Comment MODEL TO DB
+          comment
+            .save()
+            .then(post => {
+              return User.findById(req.user._id);
+            })
+            .then(comment => {
+              return Post.findById(req.params.postId);
+            })
+            .then(post => {
+              post.comments.unshift(comment);
+              return post.save();
+            })
+            .then(post => {
+              res.redirect(`/posts/${req.params.postId}`);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        }
     });
 };
--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const User = require('../models/user');
+const Comment = require('../models/comment')
 
 module.exports = app => {
 
@@ -17,37 +19,103 @@ module.exports = app => {
 
     // NEW
     app.get('/posts/new', (req, res) => {
-        res.render('posts-new');
+        var currentUser = req.user;
+        res.render('posts-new', {currentUser});
     })
 
     // CREATE
-    app.post('/posts/new', (req, res) => {
+
+    /*app.post('/posts/new', (req, res) => {
         // INSTANTIATE INSTANCE OF POST MODEL
-        const post = new Post(req.body);
+        if(req.user){
+            const post = new Post(req.body);
+            post.author = req.user._id
 
-        // SAVE INSTANCE OF POST MODEL TO DB
-        Post.create((err, post) => {
-        // REDIRECT TO THE ROOT
-            return res.redirect(`/`);
-        })
+            // SAVE INSTANCE OF POST MODEL TO DB
+            Post.create((err, post) => {
+            // REDIRECT TO THE ROOT
+                return res.redirect(`/`);
+            })
 
-        post.save();
+            post.save();
+        }*/
+
+    //});
+    app.post("/posts", (req, res) => {
+        console.log(req.user)
+        if (req.user) {
+            var post = new Post(req.body);
+            post.author = req.user._id;
+            console.log(post.author)
+
+            post
+              .save()
+              .then(post => {
+                return User.findById(req.user._id);
+              })
+              .then(user => {
+                user.posts.unshift(post);
+                user.save();
+                // REDIRECT TO THE NEW POST
+                res.redirect("/posts/" + post._id);
+              })
+              .catch(err => {
+                console.log("failed!")
+                console.log(err.message);
+              });
+        } else {
+            console.log("failed authentication!")
+            return res.status(401); // UNAUTHORIZED
+        }
     });
 
-    app.get("/posts/:id", function(req, res) {
+    //Old code with attempt to add comment authors
+    /*app.get("/posts/:id", function(req, res) {
+        var currentUser = req.user;
         // LOOK UP THE POST
         Post.findById(req.params.id).populate('comments').then((post) => {
-            res.render("post-show", { post })
+            User.findById(post.author).populate('author').then((author) => {
+                User.findById(post.comments).populate('commentAuthor').then((authorComments) => {
+                    console.log(author)
+                    res.render("post-show", { post, currentUser, author, authorComments})
+                }) .catch((err) =>{
+                    console.log(err.message)
+                })
+            }).catch((err) => {
+                console.log(err.message)
+            })
         }).catch((err) => {
             console.log(err.message)
         })
-    });
+    });*/
+
+    //Code from https://github.com/MakeFang/RedditClone
+    app.get('/posts/:postId', (req,res)=>{
+        var currentUser = req.user;
+        Post.findById(req.params.postId)
+        .populate('author', 'username')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'author',
+                model: 'User'
+            }
+        })
+        .then((post)=>{
+            console.log(post);
+            res.render('post-show', {post: post, currentUser});
+        })
+        .catch((err)=>{
+            console.log(err.message);
+        })
+    })
 
     // SUBREDDIT
     app.get("/n/:subreddit", function(req, res) {
+        var currentUser = req.user;
         Post.find({ subreddit: req.params.subreddit })
         .then(posts => {
-            res.render("posts-index", { posts });
+            res.render("posts-index", { posts , currentUser});
         })
         .catch(err => {
             console.log(err);
